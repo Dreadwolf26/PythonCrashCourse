c7232829fb156e9ec22e2ceb489349d9831741a4
Chris <chrisjholcombe@gmail.com>
2024-07-23 23:46:57 -0400

    Update Chapter8.py
    Completed more try it yourself

--- Chapter8.py
+++ Chapter8.py
@@ -33,5 +33,40 @@ describe_city("Moscow")
 describe_city("Saint Petersburg")
 describe_city("Yekaterinburg")
 
+#8-6
+def city_country(city, country):
+    cc_pair = (f"{city}, {country}")
+    print(cc_pair.title())
+
+city_country("Atlanta", "USA")
+city_country("Canton", "USA")
+city_country("Knoxville", "USA")
+
+#8-7
+def make_album(aritist_name, album_title):
+    album = {"name": aritist_name, "title": album_title}
+    print(album)
+
+make_album("Falling in Reverse", "The Drug in me is you")
+make_album("Hawthorne Heights", "The silence in black and white")
+make_album("Escape the Fate", "This war is ours")
+
+while True:
+    print("\nThis prgram will story your favorite bands and their albums you input.")
+    print("\nWhen prompted enter in the Bands name ad after enter in their album.")
+    print("\nIf you want to simply enter the word exit")
+
+    b_name = input("\nInput the name of the band you want to add")
+    if b_name == "exit":
+        break
+
+    a_name = input(f"/nInput the name of your favorite album by {b_name}")
+    if a_name == "exit":
+        break
+
+    favorite_band_and_album = make_album(b_name, a_name)
+    print(favorite_band_and_album)
+
+
 
     
